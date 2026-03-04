7a8926cd47b081c5e8d161c631ca850c397ba39a
veilbright <jmhorvath65@gmail.com>
2026-03-03 22:09:08 -0600

    Add textfile option to output text

--- main.c
+++ main.c
@@ -1,148 +1,118 @@
+#include <ctype.h>
+#include <errno.h>
+#include <getopt.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <sys/types.h>
-#include <sys/wait.h>
-#include <unistd.h>
+#include <string.h>
+#include <sys/syslog.h>
+#include <syslog.h>
 
-#include "control_center.h"
-#include "spaceship.h"
-
-/*
- * Create pipes for every pair in pipe_fds
- * size is the number of pairs in pipe_fds
- */
-void create_pipes(int (*pipe_fds)[2], const int size) {
-  for (int i = 0; i < size; i++) {
-    if (pipe(pipe_fds[i]) == -1) {
-      perror("setup_pipes failed");
-      exit(EXIT_FAILURE);
-    }
-  }
-}
+static struct option long_options[] = {
+  { "textfile", required_argument, NULL, 'f' },
+  { 0, 0, 0, 0 },
+};
 
 /*
- * Sets pids to the children's pids
- * Forks size times
- * Returns the child's index (0-size) to the children,
- * and -1 to the parent process
+ * Returns all the text from filename (removes trailing whitespace)
+ * The pointer to the text must be freed
+ * Returns NULL if there are any errors
  */
-int fork_children(pid_t* pids, const int size) {
-  for (int i = 0; i < size; ++i) {
-    pids[i] = fork();
+char* get_text(const char* filename) {
+  char* text = NULL;
 
-    // Error
-    if (pids[i] == -1) {
-      perror("fork_children failed");
-      exit(EXIT_FAILURE);
-    }
-
-    // Child
-    else if (pids[i] == 0) {
-      return i;
-    }
+  FILE* fs = fopen(filename, "r");
+  if (fs == NULL) {
+    syslog(LOG_ERR, "failed to open textfile '%s': %d\n", filename, errno);
+    return NULL;
   }
-  return -1;
-}
 
-/*
- * All pipes not at index in pipe_fds will be closed
- * size is the number of pairs in pipe_fds
- * Leaves write end of pipe at index open
- */
-void setup_child_pipes(const int (*pipe_fds)[2], const int size,
-                       const int index) {
-  for (int i = 0; i < size; ++i) {
-    // Close the read end of this child's pipe
-    if (i == index) {
-      if (close(pipe_fds[i][0]) == -1) {
-        perror("setup_child_pipes failed");
-        exit(EXIT_FAILURE);
-      }
-    }
-    // Close both ends of every other pipe
-    else {
-      if (close(pipe_fds[i][0]) == -1 || close(pipe_fds[i][1]) == -1) {
-        perror("setup_child_pipes failed");
-        exit(EXIT_FAILURE);
-      }
-    }
+  // Find text size
+  if (fseek(fs, 0, SEEK_END) == -1) {
+    syslog(LOG_ERR, "failed to fseek textfile '%s': %d\n", filename, errno);
+    goto fs_error;
   }
-}
 
-/*
- * Close the write end of the child's pipe
- */
-void close_child_pipes(const int (*pipe_fds)[2], const int index) {
-  if (close(pipe_fds[index][1]) == -1) {
-    perror("close_parent_pipes failed");
-    exit(EXIT_FAILURE);
+  long text_size = ftell(fs);
+  if (text_size == -1) {
+    syslog(LOG_ERR, "failed to find textfile '%s' offset: %d", filename, errno);
+    goto fs_error;
   }
-}
 
-/*
- * Sets up every pipe in pipe_fds to read from
- */
-void setup_parent_pipes(const int (*pipe_fds)[2], const int size) {
-  // Close write end of every pipe
-  for (int i = 0; i < size; ++i) {
-    if (close(pipe_fds[i][1]) == -1) {
-      perror("setup_parent_pipes failed");
-      exit(EXIT_FAILURE);
-    }
-  }
-}
+  // Return to start of text
+  rewind(fs);
 
-/*
- * Close any pipes that remained open after setup_parent_pipes (all read ends)
- */
-void close_parent_pipes(const int (*pipe_fds)[2], const int size) {
-  for (int i = 0; i < size; ++i) {
-    if (close(pipe_fds[i][0]) == -1) {
-      perror("close_parent_pipes failed");
-      exit(EXIT_FAILURE);
-    }
+  // Allocate text + null terminator
+  text = malloc(text_size + 1);
+  if (text == NULL) {
+    syslog(LOG_ERR, "failed to malloc char* for textfile '%s': %d", filename, errno);
+    goto fs_error;
   }
-}
 
-void wait_for_children(const pid_t child_pids[], const int size) {
-  int wstatus;
-  for (int i = 0; i < size; ++i) {
-    if (waitpid(child_pids[i], &wstatus, 0) == -1) {
-      perror("wait_for_children failed");
-      exit(EXIT_FAILURE);
-    }
+  // Read text
+  size_t read_size = fread(text, 1, text_size, fs);
+  int ferrno;
+  if ((ferrno = ferror(fs)) != 0) {
+    syslog(LOG_ERR, "failed to read text from textfile '%s': %d", filename, ferrno);
+    goto text_error;
   }
+
+  // Strip trailing whitespace
+  char* end = &text[read_size - 1];
+  while (end > text && isspace(*end)) {
+    --end;
+  }
+  *(end + 1) = '\0';
+
+  // Close file stream
+  if (fclose(fs) != 0) {
+    syslog(LOG_ERR, "failed to close textfile '%s': %d", filename, errno);
+    return NULL;
+  }
+  return text;
+
+  // F
+text_error:
+  free(text);
+
+fs_error:
+  if (fclose(fs) != 0) {
+    syslog(LOG_ERR, "failed to close textfile '%s': %d", filename, errno);
+    return NULL;
+  }
+  return NULL;
 }
 
 int main(int argc, char* argv[]) {
-  const int N = 5;  // Number of spaceships to spawn
+  const char* short_options = "f:";
+  int c_opt;
+  char* textfile;
+  char* text;
 
-  const int GOAL_DISTANCE = 1000;  // Amount of distance spaceships must travel
-  const int DELAY = 1;             // Seconds to sleep after updates
+  openlog("spacerace", LOG_CONS | LOG_PID, LOG_USER);
 
-  int pipe_fds[N][2];   // Pipe file descriptors; 1 pair per spaceship
-  pid_t child_pids[N];  // Spaceship pids
-
-  // Child variables
-  int child_index;  // Where children will store their pipe index for pipe_fds
-  struct spaceship_stats spaceship = {10, 50, 300, "\0"};
-
-  create_pipes(pipe_fds, N);
-
-  // Child (spaceship)
-  if ((child_index = fork_children(child_pids, N)) >= 0) {
-    setup_child_pipes(pipe_fds, N, child_index);
-    setup_spaceship(&spaceship, pipe_fds[child_index]);
-    race_spaceship(&spaceship, GOAL_DISTANCE, DELAY);
-    close_child_pipes(pipe_fds, child_index);
+  while ((c_opt = getopt_long(argc, argv, short_options, long_options, NULL)) != -1) {
+    switch (c_opt) {
+      case 'f':
+        textfile = strdup(optarg);
+        if (textfile == NULL) {
+          syslog(LOG_ERR, "failed to copy textfile name '%s': %d\n", optarg, errno);
+          exit(EXIT_FAILURE);
+        }
+        break;
+    }
   }
-  // Parent (control center)
+
+  // TODO: printf error checking
+  text = get_text(textfile);
+  if (text != NULL) {
+    printf("%s\n", get_text(textfile));
+  }
   else {
-    setup_parent_pipes(pipe_fds, N);
-    run_control_center(pipe_fds, N, GOAL_DISTANCE, DELAY);
-    close_parent_pipes(pipe_fds, N);
-    wait_for_children(child_pids, N);
+    printf("failed to open textfile: %s\n", textfile);
   }
+
+  free(textfile);
+  free(text);
 
   return EXIT_SUCCESS;
 }
